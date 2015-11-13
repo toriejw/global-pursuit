471d57a8024f4d2e608c08548f1a84ce620a9ea7
Nicole Hall <nicolealexandrahall@gmail.com>
2015-11-12 18:24:00 -0700

    working on slug functionality for pretty urls.

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,5 +1,23 @@
 class ActivityController < ApplicationController
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   def show
     @activity = find_activity_by_name(params[:activity_name])
+  end
+
+  def create
+    #the other option, aside from call back
+    #let the user create the name that comes
+    #in from the params
+    
+    #slug = name. paramaterize
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name)
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,7 @@
 class Activity < ActiveRecord::Base
   has_many :pursuits
 end
+
+#create slug
+#call back
+ #there is a call back called before_create
